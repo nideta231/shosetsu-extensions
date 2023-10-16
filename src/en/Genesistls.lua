b614e209b2b287e069da93f08eb48610fe331213
confident-hate <numericalpha@protonmail.com>
2023-10-16 11:34:21 +0530

    Added css and bug fix.

--- src/en/Genesistls.lua
+++ src/en/Genesistls.lua
@@ -2,6 +2,203 @@
 
 local baseURL = "https://genesistls.com"
 
+local css = [[
+    .bigcontent .infox .desc {
+        display: none
+    }
+    
+    .bigcontent .infox .mindesc {
+        display: none
+    }
+    
+    .game-prompt-info {
+        min-width: 230px;
+        box-sizing: content-box;
+        width: fit-content;
+        margin: 15px auto;
+        box-shadow: 0 0 10px 1px #00219b;
+        border-radius: 5px;
+        font-family: 'Oswald', sans-serif;
+        background-color: #000000;
+        padding: 20px 20px 10px;
+        line-height: 2;
+        font-size: 25px;
+    }
+    
+    .game-prompt-info-title {
+        letter-spacing: 2px;
+        color: #ffffff;
+        font-weight: bold;
+        text-shadow: -1px 5px 13px #004fff, -1px 0px 10px #002775;
+        border-bottom: 3px solid #fff;
+        padding-bottom: 5px;
+    }
+    
+    .game-prompt-info-title span {
+        position: relative;
+        top: -4px;
+    }
+    
+    .game-prompt-info-title span:before {
+        content: " ";
+    }
+    
+    
+    .game-prompt-info-content {
+        text-shadow: -1px 5px 13px #004fff, -1px 0px 10px #002775;
+        color: white;
+        padding-top: 10px;
+    }
+    
+    
+    .game-prompt-info-small {
+        min-width: 230px;
+        max-width: fit-content;
+        margin: 15px auto;
+        box-shadow: 0 0 10px 1px #00219b;
+        border-radius: 5px;
+        font-family: 'Oswald', sans-serif;
+        background-color: #000000;
+        padding: 10px 30px 10px 30px;
+        line-height: 2;
+        font-size: 25px;
+    }
+    
+    .game-prompt-info-small-content {
+        text-shadow: -1px 5px 13px #004fff, -1px 0px 10px #002775;
+        color: white;
+    }
+    
+    
+    .ta-center {
+        text-align: center;
+    }
+    
+    .game-prompt-warning {
+        min-width: 230px;
+        padding: 20px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        box-sizing: content-box;
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+        border: solid #750b00e3;
+        box-shadow: 0 0 20px 3px #750b0070;
+        border-radius: 5px;
+        font-family: 'Oswald', sans-serif;
+        background-color: #000000;
+        line-height: 2;
+        font-size: 25px;
+    }
+    
+    .game-prompt-warning-title {
+        letter-spacing: 2px;
+        color: #ff4348;
+        font-weight: bold;
+        text-shadow: -1px 5px 13px #ff230c, -1px 0px 10px #750b00;
+        border-bottom: 3px solid #fff;
+        padding-bottom: 5px;
+    }
+    
+    .game-prompt-warning-title span {
+        position: relative;
+        top: -4px;
+    }
+    
+    .game-prompt-warning-title span:before {
+        content: " ";
+    }
+    
+    .game-prompt-warning-content {
+        text-shadow: -1px 5px 13px #ff230c, -1px 0px 10px #750b00;
+        color: white;
+        padding-top: 10px;
+    }
+    
+    .genesistls-watermark {
+        position: fixed;
+        opacity: 0;
+    }
+    
+    p.a {
+        text-indent: 2em;
+    }
+    
+    p {
+    
+    
+        margin: 24px 0;
+        line-height: 200%;
+    }
+    
+    .game-prompt-gold-info {
+        min-width: 230px;
+        padding: 20px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        box-sizing: content-box;
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+        border: solid #e0cc00;
+        box-shadow: 0 0 10px 1px #e0cc00;
+        border-radius: 5px;
+        font-family: 'Oswald', sans-serif;
+        background-color: #000000;
+        line-height: 2;
+        font-size: 21px;
+    }
+    
+    .game-prompt-gold-info-title {
+        letter-spacing: 2px;
+        color: white;
+        font-weight: bold;
+        text-shadow: -1px 5px 13px #e0cc00, -1px 0px 10px #544d00;
+        border-bottom: 3px solid #fff;
+        padding-bottom: 5px;
+    }
+    
+    .game-prompt-gold-info-content {
+        text-shadow: -1px 5px 13px #e0cc00, -1px 0px 10px #e0cc00;
+        color: white;
+        padding-top: 10px;
+    }
+    
+    .game-prompt-silver-info {
+        min-width: 230px;
+        padding: 20px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        box-sizing: content-box;
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+        border: solid #c2c2c2;
+        box-shadow: 0 0 10px 1px #737373;
+        border-radius: 5px;
+        font-family: 'Oswald', sans-serif;
+        background-color: #000000;
+        line-height: 2;
+        font-size: 25px;
+    }
+    
+    .game-prompt-silver-info-title {
+        letter-spacing: 2px;
+        color: white;
+        font-weight: bold;
+        text-shadow: -1px 5px 13px #b4b4b4, -1px 0px 10px #737373;
+        border-bottom: 3px solid #fff;
+        padding-bottom: 5px;
+    }
+    
+    .game-prompt-silver-info-content {
+        text-shadow: -1px 5px 13px #b4b4b4, -1px 0px 10px #737373;
+        color: white;
+        padding-top: 10px;
+    }
+]]
+
 ---@param v Element
 local text = function(v)
     return v:text()
@@ -11,13 +208,13 @@ end
 ---@param url string
 ---@param type int
 local function shrinkURL(url)
-    return url:gsub("https://genesistls.com/", "")
+    return url:gsub("https://genesistls.com", "")
 end
 
 ---@param url string
 ---@param type int
 local function expandURL(url)
-    return baseURL .. "/" .. url
+    return baseURL .. url
 end
 
 --- @param chapterURL string @url of the chapter
@@ -28,9 +225,7 @@ local function getPassage(chapterURL)
     local htmlElement = htmlElement:selectFirst(".epcontent.entry-content")
     htmlElement:child(0):before("<h1>" .. title .. "</h1>");
     htmlElement:select(".genesistls-watermark"):remove()
-    -- htmlElement:select("style"):remove()
     htmlElement:select("br"):remove()
-
     local toRemove = {}
     htmlElement:traverse(NodeVisitor(function(v)
         if v:tagName() == "p" and v:text() == "" then
@@ -40,10 +235,7 @@ local function getPassage(chapterURL)
     for _,v in pairs(toRemove) do
         v:remove()
     end
-
-
-
-    return pageOfElem(htmlElement, true)
+    return pageOfElem(htmlElement, true, css)
 end
 
 --- @param data table
@@ -60,7 +252,7 @@ local function search(data)
         return Novel {
             title = v:selectFirst("a"):attr("title"),
             imageURL = v:selectFirst("a img"):attr("src"),
-            link = v:selectFirst("a"):attr("href")
+            link = shrinkURL(v:selectFirst("a"):attr("href"))
         }
     end)
 end
@@ -68,7 +260,7 @@ end
 --- @param novelURL string @URL of novel
 --- @return NovelInfo
 local function parseNovel(novelURL)
-    local url = novelURL
+    local url = expandURL(novelURL)
     local document = RequestDocument(
             RequestBuilder()
                     :get()
@@ -113,7 +305,7 @@ return {
                 return Novel {
                     title = v:select("a"):attr("title"),
                     imageURL = v:select("a img"):attr("src"),
-                    link = v:select("a"):attr("href")
+                    link = shrinkURL(v:select("a"):attr("href"))
                 }
             end)
         end)
