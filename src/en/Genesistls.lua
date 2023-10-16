90cb4ebd85a04b27469abd818afa9d15106a34de
confident-hate <numericalpha@protonmail.com>
2023-10-16 11:37:11 +0530

    version fix

--- src/en/Genesistls.lua
+++ src/en/Genesistls.lua
@@ -1,4 +1,4 @@
--- {"id":95555,"ver":"1.0.1","libVer":"1.0.0","author":"Confident-hate"}
+-- {"id":95555,"ver":"1.0.2","libVer":"1.0.0","author":"Confident-hate"}
 
 local baseURL = "https://genesistls.com"
 
