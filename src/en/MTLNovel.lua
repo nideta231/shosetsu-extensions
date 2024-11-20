d2b18ff40030f08f625812de1be9e92fa1753c49
clocks <doomsdayrs@gmail.com>
2024-11-19 20:31:32 -0500

    Fix MTLNovel.lua

--- src/en/MTLNovel.lua
+++ src/en/MTLNovel.lua
@@ -1,19 +1,9 @@
--- {"id":573,"ver":"2.0.2","libVer":"1.0.0","author":"Doomsdayrs","dep":["url>=1.0.0"]}
+-- {"id":573,"ver":"2.1.0","libVer":"1.0.0","author":"Doomsdayrs","dep":["url>=1.0.0"]}
 
 local baseURL = "https://www.mtlnovels.com"
-local settings = { [1] = 0 }
-
-local ORDER_BYS_INT = { [0] = "date",[1] = "name",[2] = "rating",[3] = "view" }
-local ORDER_BYS_KEY = 102
-
-local ORDERS_INT = { [0] = "desc",[1] = "asc" }
-local ORDERS_KEY = 103
-
-local STATUES_INT = { [0] = "all",[1] = "completed",[2] = "ongoing" }
-local STATUSES_KEY = 104
 
 local function shrinkURL(url)
-	return url:gsub("^.-mtlnovel%.com", "")
+	return url:gsub("^.-mtlnovels%.com", "")
 end
 
 local function expandURL(url)
@@ -22,6 +12,9 @@ end
 
 ---@type fun(table, string): string
 local qs = Require("url").querystring
+
+---@type dkjson
+local json = Require("dkjson")
 
 ---@param v Element | Elements
 local function text(v)
@@ -51,7 +44,7 @@ local function search(data)
 	return map(doc:select("div.search-results > div.box"),
 			function(v)
 				return Novel {
-					link = v:selectFirst("a"):attr("href"):match(baseURL .. "/(.+)/"),
+					link = shrinkURL(v:selectFirst("a"):attr("href")),
 					title = v:selectFirst(".list-title"):text(),
 					imageURL = v:selectFirst(".list-img"):attr("src")
 				}
@@ -64,14 +57,14 @@ local function parseNovel(novelURL)
 	local url = baseURL .. "/" .. novelURL
 	local document = GETDocument(url):selectFirst("article.post")
 	local n = NovelInfo()
-	n:setTitle(document:selectFirst("h1.entry-title"):text())
+	n:setTitle(document:selectFirst("h1"):text())
 	n:setImageURL(document:selectFirst("amp-img.main-tmb"):selectFirst("amp-img.main-tmb"):attr("src"))
 	n:setDescription(table.concat(map(document:selectFirst("div.desc"):select("p"), text), "\n"))
 
 	local details = document:selectFirst("table.info"):select("tr")
 	local details2 = document:select("table.info"):get(1):select("tr")
 
-	n:setAlternativeTitles({ getDetail(details:get(0)),getDetail(details:get(1)) })
+	n:setAlternativeTitles({ getDetail(details:get(0)), getDetail(details:get(1)) })
 
 	local sta = getDetailE(details:get(2)):selectFirst("a"):text()
 	n:setStatus(NovelStatus(sta == "Completed" and 1 or sta == "Ongoing" and 0 or 3))
@@ -89,7 +82,7 @@ local function parseNovel(novelURL)
 		local chaptersList = AsList(map(chapters, function(v)
 			local c = NovelChapter()
 			c:setTitle(v:text():gsub("<strong>", ""):gsub("</strong>", " "))
-			c:setLink(v:attr("href"):match(baseURL .. "/(.+)/?$"))
+			c:setLink(shrinkURL(v:attr("href"):match(baseURL .. "/(.+)/?$")))
 			c:setOrder(count)
 			count = count - 1
 			return c
@@ -115,6 +108,20 @@ local function getPassage(chapterURL)
 	return pageOfElem(htmlElement, true)
 end
 
+local function parseItem(item)
+	local a = Document(item.novel_permalink):selectFirst("a")
+	return Novel {
+		imageURL = item.tmb,
+		link = shrinkURL(a:attr("href")),
+		title = a:text()
+	}
+end
+
+local function getLatest(data)
+	local response = json.GET(baseURL .. "/wp-admin/admin-ajax.php?action=rcnt_update&moreItemsPageIndex=" .. data[PAGE])
+	return map(response.items, parseItem)
+end
+
 return {
 	id = 573,
 	name = "MTLNovel",
@@ -125,36 +132,12 @@ return {
 
 	shrinkURL = shrinkURL,
 	expandURL = expandURL,
+	startIndex = 0,
 
 	listings = {
-		Listing("Novel List", true, function(data)
-			local d = GETDocument(baseURL .. "/novel-list/" ..
-					"?orderby=" .. ORDER_BYS_INT[data[ORDER_BYS_KEY]] ..
-					"&order=" .. ORDERS_INT[data[ORDERS_KEY]] ..
-					"&status=" .. STATUES_INT[data[STATUSES_KEY]] ..
-					"&pg=" .. data[PAGE])
-			return map(d:select("div.box.wide"), function(v)
-				local lis = Novel()
-				lis:setImageURL(v:selectFirst("amp-img.list-img"):selectFirst("amp-img.list-img"):attr("src"))
-				local title = v:selectFirst("a.list-title")
-				lis:setLink(title:attr("href"):match(baseURL .. "/(.+)/"))
-				lis:setTitle(title:attr("aria-label"))
-				return lis
-			end)
-		end)
+		Listing("Latest", true, getLatest)
 	},
 	getPassage = getPassage,
 	parseNovel = parseNovel,
-	--settings = {
-		--  DropdownFilter(1, "Language", { "English", "Chinese" })
-	--},
-	searchFilters = {
-		DropdownFilter(ORDER_BYS_KEY, "Order by", { "Date","Name","Rating","Views" }),
-		DropdownFilter(ORDERS_KEY, "Order", { "Descending","Ascending" }),
-		DropdownFilter(STATUSES_KEY, "Status", { "All","Completed","Ongoing" })
-	},
-	--updateSetting = function(id, value)
-	--	settings[id] = value
-	--end
 	search = search
 }
